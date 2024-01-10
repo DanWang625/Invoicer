fd1c19053c12a3ab3e347958cd85f79b9aa1e819
Dan Wang <danwang625@gmail.com>
2024-01-10 09:37:06 -0800

    updating an invoice

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -3,6 +3,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  CurrencyDollarIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -18,6 +19,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Expenses', href: '/dashboard/expenses', icon: CurrencyDollarIcon},
 ];
 
 export default function NavLinks() {
